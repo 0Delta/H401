975b3f96273a63c5b11656fbd42612b3ba412294
keyage <dancekeybeat@gmail.com>
2015-10-22 01:29:03 +0900

    ・ノードをタップ時のアクションを作成

---
+++ H401/Assets/Scripts/Node.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using UniRx;
+using UnityEngine.UI;
+
+public class Node : MonoBehaviour {
+
+    private const float ROT_HEX_ANGLE = 60.0f;      // 六角形パネルの回転角度
+
+    [SerializeField] private float actionTime = 0.0f;       // アクションにかかる時間
+    [SerializeField] private float scaleSize  = 0.0f;       // タップ時の拡大サイズ
+
+    private bool isAction = false;      // アクションフラグ
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnMouseDown() {
+        // アクション中なら未処理
+        if(isAction)
+            return;
+
+        // アクション開始
+        isAction = true;
+        
+        // 回転準備
+        float angle = transform.localEulerAngles.z - ROT_HEX_ANGLE;
+
+        // 回転処理(※TODO:new をなんとかしたい)
+        transform.DORotate(new Vector3(0.0f, 0.0f, angle), actionTime)
+            .OnComplete(() => {
+                // 回転成分を初期化
+                if(angle <= -360.0f) {
+                    transform.rotation = Quaternion.identity;
+                }
+
+                // アクション終了
+                isAction = false;
+            });
+
+        // 拡縮処理
+        transform.DOScale(scaleSize, actionTime * 0.5f).SetLoops(2, LoopType.Yoyo);
+    }
+}
---
+++ H401/Assets/Scripts/NodeController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NodeController : MonoBehaviour {
+
+    private const float ADJUST_PIXELS_PER_UNIT = 0.01f;     // Pixels Per Unit の調整値
+
+    [SerializeField] private int row = 0;       // 横配置数
+    [SerializeField] private int col = 0;       // 縦配置数
+    [SerializeField] private GameObject nodePrefab = null;       // パネルのプレハブ
+
+	// Use this for initialization
+	void Start () {
+        // 描画するパネルの大きさを取得
+        float width  = nodePrefab.GetComponent<SpriteRenderer>().sprite.texture.width * nodePrefab.transform.localScale.x * ADJUST_PIXELS_PER_UNIT;
+        float height = nodePrefab.GetComponent<SpriteRenderer>().sprite.texture.height * nodePrefab.transform.localScale.y * ADJUST_PIXELS_PER_UNIT;
+        Vector3 pos = transform.position;
+
+        // パネルを生成
+        for(int i = 0; i < col; ++i) {
+            // パネルの配置位置を調整(Y座標)
+            pos.y = transform.position.y + height * (col * 0.5f - (i + 0.5f));
+
+            for (int j = 0; j < row; ++j) {
+                // パネルの配置位置を調整(X座標)
+                pos.x = transform.position.x + width * -(row * 0.5f - (j + 0.5f));
+
+                // 生成
+        	    Instantiate(nodePrefab, pos, transform.rotation);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
