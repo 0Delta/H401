e161a6164573572d9f8403a1d7db98ec19b123f1
keyage <dancekeybeat@gmail.com>
2015-10-28 14:15:34 +0900

    ・int型のVector2構造体を追加

--- H401/Assets/Scripts/Utils/Structs.cs
+++ H401/Assets/Scripts/Utils/Structs.cs
@@ -1,1 +1,26 @@
-﻿
+﻿// int型のVector2構造体
+public struct Vec2Int
+{
+    public int x;
+    public int y;
+
+    public Vec2Int(int _x, int _y) {x = _x; y = _y;}
+
+    public static Vec2Int operator + (Vec2Int a, Vec2Int b) {
+        return new Vec2Int(a.x + b.x, a.y + b.y);
+    }
+    public static Vec2Int operator - (Vec2Int a, Vec2Int b) {
+        return new Vec2Int(a.x - b.x, a.y - b.y);
+    }
+    public static Vec2Int operator * (Vec2Int a, Vec2Int b) {
+        return new Vec2Int(a.x * b.x, a.y * b.y);
+    }
+    public static Vec2Int operator / (Vec2Int a, Vec2Int b) {
+        return new Vec2Int(a.x / b.x, a.y / b.y);
+    }
+
+    public override string ToString()
+    {
+        return "(" + x.ToString() + ", " + y.ToString() + ")";
+    }
+};
