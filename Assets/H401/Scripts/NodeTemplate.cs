c5d3f3f2245776e103af8025240d929d6d581399
Tomoki_Seno@0Delta <0deltast@gmail.com>
2016-01-13 19:36:52 +0900

    スプライトのインデックス用変数を追加

--- Assets/H401/Scripts/NodeTemplate.cs
+++ Assets/H401/Scripts/NodeTemplate.cs
@@ -5,8 +5,9 @@ using System.Collections;
 public class NodeTemplate {
 
     // 設定部分
-    static public string SpriteName;
-
+    static public string SpriteName = null;
+    public int SpriteIdx = -1;
+    public int MaskIdx = -1;
 
     public string MaterialName;
     public bool[] LinkDir = new  bool[6];
