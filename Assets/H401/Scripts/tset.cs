af304e127baaf2adbc4a4dd9c65022993046ee3d
keyage <dancekeybeat@gmail.com>
2016-01-16 20:16:45 +0900

    ・キラキラエフェクトが一定以上出なくなるバグを解消 ・キラキラエフェクトが消える処理を更新

---
+++ Assets/H401/Scripts/tset.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tset : MonoBehaviour {
+    
+    public float effectMoveDurationTime = 0.0f;
+    public float effectMoveWaitTime = 0.0f;
+
+    private float courceTime = 0.0f;
+    private float startSize;
+
+    void Start() {
+        startSize = transform.GetComponent<ParticleSystem>().startSize;
+    }
+
+    void Update() {
+        ParticleSystem ps = transform.GetComponent<ParticleSystem>();
+
+        if(courceTime > effectMoveDurationTime) {
+            float time = courceTime - effectMoveDurationTime;
+            ps.startSize = Mathf.Lerp(startSize, 0.0f, time / effectMoveWaitTime);
+        }
+
+        courceTime += Time.deltaTime;
+        
+        if(courceTime > effectMoveDurationTime + effectMoveWaitTime) {
+            courceTime = 0.0f;
+            ps.startSize = startSize;
+        }
+    }
+}
