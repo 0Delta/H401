d564b9a30e2e0556bbfa851a407bb507995b3115
keyage <dancekeybeat@gmail.com>
2016-01-20 17:30:03 +0900

    ・遊び方を実装 ・BGMをマスター版に差し替え

---
+++ Assets/H401/Scripts/GameScene/PseudoGridLight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class PseudoGridLight : MonoBehaviour {
+
+    [SerializeField] private float endPosY = 0.0f;
+    [SerializeField] private float moveTime = 0.0f;
+    [SerializeField] private float startWaitTime = 0.0f;
+
+	// Use this for initialization
+	IEnumerator Start () {
+        yield return new WaitForSeconds(startWaitTime);
+
+        transform.DOLocalMoveY(endPosY, moveTime)
+            .SetLoops(-1, LoopType.Restart)
+            .SetEase(Ease.Linear);
+	}
+}
