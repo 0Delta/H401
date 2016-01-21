6a4e3b2e41c593c048507c7957695ef868692393
GotoK <tutohanp@gmail.com>
2016-01-22 02:01:38 +0900

    いろんなボタンにSEがついていなかったのをつけた オプションのSEスライダーを離したときにSEが鳴るように

---
+++ Assets/TapPlaySE.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TapPlaySE : MonoBehaviour {
+
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+
+
+	void OnMouseUp () {
+        audioSource.Play();
+	}
+}
