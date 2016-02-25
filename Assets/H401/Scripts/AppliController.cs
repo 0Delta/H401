ebfdbcd1f52dee2defe3a94513114d5a47ab764d
GotoK <tutohanp@gmail.com>
2016-02-25 11:08:46 +0900

    オプション情報を保存するように

--- Assets/H401/Scripts/AppliController.cs
+++ Assets/H401/Scripts/AppliController.cs
@@ -41,6 +41,8 @@ public class AppliController : MonoBehaviour {
     private Fade       fade;                    // シーン切り替え時の演出用 Script
     private GameObject tapKirakiraPrefab;       // タップ時のキラキラエフェクトの Prefab
 
+
+
     private bool _gyroEnable;
     public bool gyroEnable { set { _gyroEnable = value; } get { return _gyroEnable; } }
 
@@ -62,7 +64,12 @@ public class AppliController : MonoBehaviour {
 	void Start() {
         fade = transform.FindChild(fadeCanvasName).gameObject.GetComponentInChildren<Fade>();
 
+
         _gyroEnable = true;//ここにファイル読み込みからのセットを
+
+        OptionIO opIO = GetComponent<OptionIO>();
+        opIO.appController = this;
+        opIO.load();
 
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = gameFrameRate;  //シーンごとにFPSを設定するべきらしい
